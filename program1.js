f440f1c6384b3021dffdee2c59ddce42493444fc
yashallabadi <yashallabadi24@gmail.com>
2024-04-25 13:27:31 +0530

    done and all test cases pass in both program

--- program1.js
+++ program1.js
@@ -1,7 +1,18 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    const charIndexMap = {};
+    let maxLength = 0;
+    let start = 0;
+
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s[end];
+        if (currentChar in charIndexMap && charIndexMap[currentChar] >= start) {
+            start = charIndexMap[currentChar] + 1;
+        }
+        charIndexMap[currentChar] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
-
-
--- program2.js
+++ program2.js
@@ -1,7 +1,20 @@
 function smallestMissingPositiveInteger(nums) {
+  const n = nums.length;
+  
+  for (let i = 0; i < n; i++) {
+      while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+          let correctPos = nums[i] - 1;
+          [nums[i], nums[correctPos]] = [nums[correctPos], nums[i]]; // swap elements
+      }
+  }
 
-  // Implement the function smallest_missing_positive_integer
+  for (let i = 0; i < n; i++) {
+      if (nums[i] !== i + 1) {
+          return i + 1;
+      }
+  }
 
+  return n + 1;
 }
 
 module.exports = smallestMissingPositiveInteger;
